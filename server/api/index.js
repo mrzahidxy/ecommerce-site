083922b9caddee3bee2d6e9ada5681a7af32898c
Md. Zahid Hasan <mrzahidxy@gmail.com>
2024-06-13 21:38:16 +0600

    modify: serveless function

--- server/api/index.js
+++ server/api/index.js
@@ -1,40 +1,4 @@
-const express = require('express');
-const connectDB = require('./config/db')
-const dotenv = require('dotenv')
-const authRouter = require('./Routes/auth');
-const userRouter = require('./Routes/user');
-const productRouter = require('./Routes/product')
-const cartRouter = require('./Routes/cart')
-const oderRouter = require('./Routes/order')
-const stripeRouter = require('./Routes/stripe')
-const cors = require('cors')
+const app = require('./app');
+const serverless = require('serverless-http');
 
-dotenv.config();
-
-// Initialize Express app
-const app = express();
-
-
-// Database connected
-connectDB()
-
-// Middleware
-app.use(express.json())
-app.use(cors())
-
-
-// Routing
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/carts', cartRouter);
-app.use('/api/orders', oderRouter)
-app.use('/api/checkout', stripeRouter)
-
-app.get('/', (req, res)=>res.send("Home"))
-
-app.listen(process.env.PORT || 8080, () => {
-    console.log('Backend is running');
-})
-
-module.exports = app
+module.exports = serverless(app);
